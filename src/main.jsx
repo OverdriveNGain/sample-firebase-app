840d21b5d21ba96f6487a9d6435c689a97779280
Jeremy Mattheu D. Amon <jeremy.amon@paymaya.com>
2024-11-04 15:25:04 +0800

    Added firebase config to gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from "./components/app.jsx";
 import "./index.css";
+import { getFirestore } from "firebase/firestore";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase.config.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+export const firestore = getFirestore();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
